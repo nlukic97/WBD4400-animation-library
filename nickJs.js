5fef48dbecc73265d586bc683d91f78640f74d41
nlukic97 <nlukic97@gmail.com>
2020-04-23 16:40:38 +0200

    added more animations

--- nickJs.js
+++ nickJs.js
@@ -1,7 +1,7 @@
 
 /*https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/*/
 
-// animation 4 js
+// animation 3 js
 $(window).scroll(function(){
 	var item = $('.nickStyle.item');
 	$(item).each(function(index, theSection){
@@ -10,6 +10,20 @@ $(window).scroll(function(){
 		var height = window.innerHeight;
 		if(bounding < height * 0.60 || bounding < height * 0.90){
 				$(theSection).attr('id','show');
+			}
+		})
+})
+
+//adding tilt effect when element is in screen. To use, add classes ='nickStyle addTilt.
+//    when element reaches half the window height, it will have a cool animation
+$(window).scroll(function(){
+	var item = $('.nickStyle.addTilt');
+	$(item).each(function(index, theElement){
+
+		var bounding = theElement.getBoundingClientRect().y; //distance of top of section from top of page
+		var height = window.innerHeight;
+		if(bounding < height * 0.60 || bounding < height * 0.50){
+				$(theElement).addClass('tilt')
 			}
 		})
 })
